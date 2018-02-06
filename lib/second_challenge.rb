6c5b9c7bbc2b62f8d9732a04c1b7805add3a8089
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-06 21:59:57 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,13 +3,13 @@ def second_challenge
     :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
       :matriarch => {:name => "Lady Montague", :age => "54"},
-      :hero => {}, 
+      :hero => {:name => "Romeo", :age => "15", :statue => "alive"}, 
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {}, 
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lady Capulet", :age => "51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"}, 
       :heroine_friends => []
       }}
 
