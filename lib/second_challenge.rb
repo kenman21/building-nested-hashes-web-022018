0714eef70b8dd0a0c17661fd91bcfa6d9ef2be87
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-06 21:58:03 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
       :hero => {}, 
       :hero_friends => []
       }, 
